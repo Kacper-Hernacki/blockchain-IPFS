e92b16c896b16227aee40a90711df7d2f35301c8
Kacper <hernackikacper@gmail.com>
2021-08-17 16:53:02 +0200

    blockchain setup

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
-import logo from '../logo.png';
-import './App.css';
+import React, { Component } from 'react'
+import logo from '../logo.png'
+import './App.css'
 
 class App extends Component {
   render() {
@@ -9,43 +9,21 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://www.thedevsuniverse.com/"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+            TheDev'sUniverse
           </a>
         </nav>
         <div className="container-fluid mt-5">
           <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  <img src={logo} className="App-logo" alt="logo" />
-                </a>
-                <h1>Dapp University Starter Kit</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
-              </div>
-            </main>
+            <main role="main" className="col-lg-12 d-flex text-center"></main>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
